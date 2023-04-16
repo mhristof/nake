2de1281f0764ed663962fd75e02aefb373532d61
Mike Christofilopoulos <mhristof@gmail.com>
2023-04-16 13:19:05 +0100

    fix: dont deploy template files if they exist

--- nake/templates/__init__.py
+++ nake/templates/__init__.py
@@ -16,33 +16,36 @@ def files(directory):
     remote = git_remote(directory)
     log.debug("Remote: %s", remote)
 
-    files = render("default")
+    files = render("default", directory)
 
     if "-infra" in remote:
-        files.update(render("terraform"))
+        files.update(render("terraform", directory))
 
     if "docker-" in remote:
-        files.update(render("docker"))
+        files.update(render("docker", directory))
 
     return files
 
 
-def render(folder):
+def render(folder, directory=""):
     ret = {}
 
     template_dir = os.path.join(os.path.dirname(__file__), folder)
 
     log.debug("Reading templates from: %s", template_dir)
 
-    for root, directory, files in os.walk(template_dir):
+    for root, _, files in os.walk(template_dir):
         log.debug("root: %s", root)
 
         for file in files:
             path = os.path.join(root, file)
-
-            log.debug("Reading file: %s %s", root, file)
-
             dest = os.path.join(root, file).replace(template_dir + "/", "")
+
+            if os.path.exists(os.path.join(directory, dest)):
+                log.debug("Skipping file: %s", dest)
+
+                continue
+
             with open(path, "r") as stream:
                 ret[dest] = stream.read()
 
