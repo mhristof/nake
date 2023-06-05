cb3e5be8c82e59f16a8931fecd3148897588f70e
Mike Christofilopoulos <mhristof@gmail.com>
2023-06-05 14:46:31 +0100

    Revert "feat: deploy tf changes based on labels"
    This reverts commit 45ec23fcecc2acc372768a1de1f07439d824033e.

--- nake/gitlabci/__init__.py
+++ nake/gitlabci/__init__.py
@@ -246,14 +246,6 @@ def terraform(config, features):
                 "entrypoint": ["/bin/sh", "-c"],
             },
             "before_script": before_script,
-            "rules": [
-                {"if": "$CI_MERGE_REQUEST_LABELS =~ /terraform-auto-approve/"},
-                {
-                    "if": "$CI_PIPELINE_SOURCE == 'merge_request_event'",
-                    "when": "manual",
-                },
-                {"if": "$CI_PIPELINE_SOURCE == 'push'", "when": "manual"},
-            ],
         },
     }
 
@@ -359,9 +351,6 @@ apply:
                 **{"AWS_REGION": region},
             }
             log.debug("Setting AWS_REGION to %s", region)
-
-        if name == "test":
-            plan["apply"].pop("when")
 
         config = {
             **config,
